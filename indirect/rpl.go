92a6de47ad069e91af25d1a5d66559dc1213a54f
James Craig Burley <james-github@burleyarch.com>
2018-11-18 18:55:03 -0500

    Get this working using a bufio.Reader instead of bufio.Scanner

--- indirect/rpl.go
+++ indirect/rpl.go
@@ -5,10 +5,29 @@ import (
 	"fmt"
 )
 
+func readLine(r *bufio.Reader) (s string, err error) {
+	for {
+		line, isPrefix, err := r.ReadLine();
+		if err != nil {
+			return "", err
+		}
+		s += string(line)
+		if !isPrefix {
+			break
+		}
+		fmt.Fprintf(Stderr, "!isPrefix after %d runes in string!\n", len(s))
+	}
+	return
+}
+
 func rpl() {
-	rd := bufio.NewScanner(Stdin)
-	for rd.Scan() {
-		line := rd.Text()
+	r := bufio.NewReader(Stdin)
+	for {
+		line, err := readLine(r)
+		if err != nil {
+			fmt.Fprintf(Stderr, "err=%v\n", err)
+			return
+		}
 		if len(line) > 0 && line[0] == 'E' {
 			fmt.Fprintf(Stderr, "to stderr: %s\n", line[1:])
 		} else if line == "." {
@@ -16,8 +35,5 @@ func rpl() {
 		} else {
 			fmt.Fprintf(Stdout, "to stdout: %s\n", line)
 		}
-	}
-	if err := rd.Err(); err != nil {
-		fmt.Fprintf(Stderr, "err=%v\n", err)
 	}
 }
